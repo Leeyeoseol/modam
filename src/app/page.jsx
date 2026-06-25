bff81ab87e90baafa1c53b2220734294da203603
Leeyeoseol <iyeoseol096@gmail.com>
2026-06-25 18:21:04 +0900

    feat: 방 상세 페이지 및 방 코드 참여 구현 (#5)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,6 +18,7 @@ export default function Home() {
       <div>
         <h1>MODAM</h1>
         <Link href="/room/create">방 만들기</Link>
+        <Link href="/room/join">방 참여</Link>
         <button onClick={handleLogout}>로그아웃</button>
       </div>
     </main>
